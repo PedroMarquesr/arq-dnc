2efcef89d80ec3df58f054949e583dc15dc29b2a
Pedro Marques <pedro.marques3005@live.com>
2025-09-23 23:27:05 -0300

    Inicia projeto

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <h1>Home</h1>
+      <p>Subtitle</p>
+      <Footer />
+    </>
+  );
+}
